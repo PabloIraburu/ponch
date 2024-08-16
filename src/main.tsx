8e7a0d1f1ff5b226f0e849b74b07f4a77c8d59cd
PabloIraburu <Iraburulopez@bloobirds.com>
2024-08-16 10:29:23 +0200

    fix: redirect to movies

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import {EntityView} from "./pages/entityView.tsx";
 import Dashboard from "./pages/dashboards/Dashboard.tsx";
-import {Movies} from "./pages/movies/movies.tsx";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
     <Dashboard />
