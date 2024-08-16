8d073a5371edde5794828a60ecd84802c6674fe5
PabloIraburu <Iraburulopez@bloobirds.com>
2024-08-16 10:28:04 +0200

    fix: redirect to movies

--- src/main.tsx
+++ src/main.tsx
@@ -7,23 +7,6 @@ import {EntityView} from "./pages/entityView.tsx";
 import Dashboard from "./pages/dashboards/Dashboard.tsx";
 import {Movies} from "./pages/movies/movies.tsx";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (<Dashboard />),
-        children: [
-            {
-                path: "/:entity",
-                element: <EntityView/>,
-            },
-        ]
-    },
-    {
-        path: "/movies",
-        element: <Movies/>,
-    },
-]);
-
 createRoot(document.getElementById("root") as HTMLElement).render(
-    <RouterProvider router={router} />
+    <Dashboard />
 );
--- src/pages/dashboards/Dashboard.tsx
+++ src/pages/dashboards/Dashboard.tsx
@@ -21,18 +21,54 @@ import { mainListItems, secondaryListItems } from './listItems';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {EntityView} from "../entityView.tsx";
+import {Movies} from "../movies/movies.tsx";
 
-function Copyright(props: any) {
+
+const DashboardContent = () => {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+      <>
+      <Toolbar />
+  <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+    <Grid container spacing={3}>
+      {/* Chart */}
+      <Grid item xs={12} md={8} lg={9}>
+        <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 240,
+            }}
+        >
+          <Chart />
+        </Paper>
+      </Grid>
+      {/* Recent Deposits */}
+      <Grid item xs={12} md={4} lg={3}>
+        <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 240,
+            }}
+        >
+          <Deposits />
+        </Paper>
+      </Grid>
+      {/* Recent Orders */}
+      <Grid item xs={12}>
+        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+          <Orders />
+        </Paper>
+      </Grid>
+    </Grid>
+  </Container>
+  </>
+
+  )
 }
 
 const drawerWidth: number = 240;
@@ -85,6 +121,24 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<DashboardContent />),
+    ErrorBoundary: ({ error }) => <div>{error?.message}</div>,
+    errorElement: <div>There was an error</div>,
+    children: [
+      {
+        path: "/:entity",
+        element: <EntityView/>,
+      },
+    ]
+  },
+  {
+    path: "/movies",
+    element: <Movies/>,
+  },
+]);
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
@@ -164,44 +218,7 @@ export default function Dashboard() {
             overflow: 'auto',
           }}
         >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Chart />
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Deposits />
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Orders />
-                </Paper>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ pt: 4 }} />
-          </Container>
+        <RouterProvider router={router} />
         </Box>
       </Box>
     </ThemeProvider>
